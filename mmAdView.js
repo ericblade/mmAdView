faef7f25b75a3e46674f1938f9628a4444ea1495
Rylan Cottrell <rylan@icottrell.com>
2012-08-01 20:04:56 -0600

    Update mmAdView.js
    Updated to work in enyo 2.0

--- mmAdView.js
+++ mmAdView.js
@@ -1,6 +1,6 @@
 enyo.kind({
     name: "MillenialMedia.AdView",
-    kind: "VFlexBox",
+    kind: "Control",
     published: {
         /* Documented metadata as I write this is:
          * age, gender, zip, marital, income, lat, long
@@ -15,29 +15,48 @@ enyo.kind({
     uip: undefined, /* User's IP Address will be stored here */
     components:
     [
-        { name: "getIP", kind: "WebService", url: "http://jsonip.com", handleAs: "json", onSuccess: "receivedIP", onFailure: "failedIP" },
-        { name: "getAd", kind: "WebService", url: this.adserver, handleAs: "text", onSuccess: "receivedAd", onFailure: "failedAd", },
-        { name: "webView", kind: "Control", flex: 1, allowHtml: true, },
+       { name: "webView", kind: "Control", fit: true, allowHtml: true, },
     ],
     create: function()
     {
         this.inherited(arguments);
-        this.$.getIP.call();
+        this.getIP();
         this.ua = navigator.userAgent;
+    },
+    getIP: function() {
+        var request = new enyo.Ajax({
+            url: "http://jsonip.com",
+            handleAs: "json"
+        });
+
+        request.response(enyo.bind(this, this.receivedIP));
+        request.go();
+    },
+    getAd: function(inUrl) {
+        var u, request;
+        if(inUrl){
+            u = inUrl;
+        } else {
+            u = this.adserver;
+        }
+        request = new enyo.Ajax({
+         url: u ,
+            handleAs: "text"
+        });
+
+        request.response(enyo.bind(this, this.receivedAd));
+        request.go();
     },
     failedIP: function(x, y, z) { this.log(x, ",", y, ",", z); },
     failedAd: function(x, y, z) { this.log(x, ",", y, ",", z); },
     receivedIP: function(inSender, inResponse, inRequest)
     {
-        var deviceInfo = enyo.fetchDeviceInfo();
         this.uip = inResponse.ip;
         // It appears that they do some kind of User-Agent filtering for desktop browsers.
         // We want to be able to test, or even run the code in desktop browsers, SO
         // we'll supply a TouchPad UA, since that is known to work, if our fetchDeviceInfo
         // call fails.
-        if(deviceInfo) {
-            this.auid = deviceInfo.serialNumber;
-        } else if(window.PhoneGap || window.Cordova) {
+        if(window.PhoneGap || window.Cordova) {
             this.auid = device.uuid;
         } else {
             this.auid = "DEVUID";
@@ -51,7 +70,7 @@ enyo.kind({
         var str = "?";
         if(!this.uip)
         {
-            this.$.getIP.call();
+            this.getIP();
             return;
         }
         params = {
@@ -61,13 +80,11 @@ enyo.kind({
             "uip": this.uip,
         };
         enyo.mixin(params, this.metadata);
-        this.$.getAd.setUrl("http://" + this.adserver + "/getAd");
         for(var x in params) {
             str += x + "=" + encodeURI(params[x]);
             str += "&";
         }
-        this.log(this.$.getAd.url + str);
-        this.$.getAd.call(params);
+        this.getAd("http://" + this.adserver + "/getAd"+str);
     },
     receivedAd: function(inSender, inResponse, inRequest)
     {
